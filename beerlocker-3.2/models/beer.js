d36ca856ab75e11f66638338a6581e6ab8cd899f
Scott Smith <scottksmith95@gmail.com>
2014-05-29 10:10:40 -0700

    Auth controller added

---
+++ beerlocker-3.2/models/beer.js
@@ -0,0 +1,12 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our beer schema
+var BeerSchema   = new mongoose.Schema({
+  name: String,
+  type: String,
+  quantity: Number
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Beer', BeerSchema);
---
+++ beerlocker-3.2/server.js
@@ -0,0 +1,45 @@
+// Load required packages
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
+var authController = require('./controllers/auth');
+
+// Connect to the beerlocker MongoDB
+mongoose.connect('mongodb://localhost:27017/beerlocker');
+
+// Create our Express application
+var app = express();
+
+// Use the body-parser package in our application
+app.use(bodyParser());
+
+// Use the passport package in our application
+app.use(passport.initialize());
+
+// Create our Express router
+var router = express.Router();
+
+// Create endpoint handlers for /beers
+router.route('/beers')
+  .post(authController.isAuthenticated, beerController.postBeers)
+  .get(authController.isAuthenticated, beerController.getBeers);
+
+// Create endpoint handlers for /beers/:beer_id
+router.route('/beers/:beer_id')
+  .get(authController.isAuthenticated, beerController.getBeer)
+  .put(authController.isAuthenticated, beerController.putBeer)
+  .delete(authController.isAuthenticated, beerController.deleteBeer);
+
+// Create endpoint handlers for /users
+router.route('/users')
+  .post(userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers);
+
+// Register all our routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(3000);
