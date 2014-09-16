61e76f3a49719e6b7f271d14b6fd64851ae93261
Scott Smith <scottksmith95@gmail.com>
2014-09-16 08:27:51 -0700

    Final code for part 6.2

--- beerlocker-6.2/controllers/auth.js
+++ beerlocker-6.2/controllers/auth.js
@@ -2,6 +2,7 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var DigestStrategy = require('passport-http').DigestStrategy;
+var LocalStrategy = require('passport-local').Strategy;
 var BearerStrategy = require('passport-http-bearer').Strategy;
 var User = require('../models/user');
 var Client = require('../models/client');
@@ -48,6 +49,31 @@ passport.use(new DigestStrategy(
   }
 ));
 
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'pass'
+  },
+  function(username, password, callback) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return callback(err); }
+
+      // No user found with that username
+      if (!user) { return callback(null, false); }
+
+      // Make sure the password is correct
+      user.verifyPassword(password, function(err, isMatch) {
+        if (err) { return callback(err); }
+
+        // Password did not match
+        if (!isMatch) { return callback(null, false); }
+
+        // Success
+        return callback(null, user);
+      });
+    });
+  }
+));
+
 passport.use('client-basic', new BasicStrategy(
   function(username, password, callback) {
     Client.findOne({ id: username }, function (err, client) {
@@ -83,6 +109,6 @@ passport.use(new BearerStrategy(
   }
 ));
 
-exports.isAuthenticated = passport.authenticate(['basic', 'bearer'], { session : false });
+exports.isAuthenticated = passport.authenticate(['local', 'bearer'], { session : false });
 exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false });
 exports.isBearerAuthenticated = passport.authenticate('bearer', { session: false });
